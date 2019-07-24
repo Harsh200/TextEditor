17cb23fd5f51b435a08ae7022fb2f1d97c1303c5
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 13:47:11 +0530

    defining the save and open command in the fileMenu

--- TextEditor.py
+++ TextEditor.py
@@ -4,5 +4,6 @@ mainMenu=Menu(window)
 window.config(menu=mainMenu)
 fileMenu=Menu(mainMenu)
 mainMenu.add_cascade(label="File",menu=fileMenu)
-
+fileMenu.add_command(label="Open",command=openFile)
+fileMenu.add_command(label="save",command=saveFile)
 window.mainloop()
