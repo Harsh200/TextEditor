18d4c63bf47562ee223928505821b938bd14af05
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 13:50:09 +0530

    defining the text area for writing

--- TextEditor.py
+++ TextEditor.py
@@ -8,4 +8,8 @@ fileMenu.add_command(label="Open",command=openFile)
 fileMenu.add_command(label="save",command=saveFile)
 fileMenu.add_separator()
 fileMenu.add_command(label="Close")
+
+mainMenu.add_command(label="Help")
+
+content=Text(window,width=100)
 window.mainloop()
