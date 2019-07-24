c645f24cc67308199419aec1c2a7c82ad0087204
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 14:40:53 +0530

    grid the SizeBox

--- TextEditor.py
+++ TextEditor.py
@@ -39,13 +39,14 @@ def optionClick():
 
     sizeBox=Listbox(optionWindow)
     for i in range(8,88,4):
-
+        sizeBox.insert(END,i)
 
 
     lbfont.grid(row=0,column=0,padx=10,pady=5)
     lbsize.grid(row=0, column=1, padx=10, pady=5)
     lbtype.grid(row=0, column=2, padx=10, pady=5)
     fontBox.grid(row=1,column=0,padx=10,pady=5)
+    sizeBox.grid(row=1,column=1,padx=10,padx=5)
     optionWindow.mainloop()
 
 window=Tk()
