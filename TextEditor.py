855216c5eb3b16a90a7d27ecb8a98d45b94b7689
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 14:44:38 +0530

    defining the combo box for typeOption windpw

--- TextEditor.py
+++ TextEditor.py
@@ -46,7 +46,7 @@ def optionClick():
     for i in range(8,88,4):
         sizeBox.insert(END,i)
     typeOption=("normal","bold","normal italic","bold italic")
-
+    cbxType=ttk.Combobox(optionWindow,values=typeOption)
 
     lbfont.grid(row=0,column=0,padx=10,pady=5)
     lbsize.grid(row=0, column=1, padx=10, pady=5)
